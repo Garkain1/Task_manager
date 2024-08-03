5cd53287e32843ca0e0f2a00edf576ae92b8107f
Sergei Oskolkov <SergeyOsk2017@gmail.com>
2024-08-04 00:03:06 +0200

    Initial commit

---
+++ tasks/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Task, SubTask, Category
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'deadline', 'created_at')
+    list_filter = ('status', 'created_at', 'deadline')
+    search_fields = ('title', 'description')
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'deadline', 'created_at', 'task')
+    list_filter = ('status', 'created_at', 'deadline')
+    search_fields = ('title', 'description')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
