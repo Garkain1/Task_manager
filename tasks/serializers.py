a43f269f2422fa3c660e1f46e99f7057401fb1c4
Sergei Oskolkov <SergeyOsk2017@gmail.com>
2024-08-18 17:16:29 +0200

    Home Work №12

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,8 +1,63 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, SubTask, Category
+from rest_framework.exceptions import ValidationError
+from django.utils import timezone
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+
+class SubTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTask
+        fields = '__all__'
+
+
+class TaskDetailSerializer(serializers.ModelSerializer):
+    subtasks = SubTaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class TaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+    def validate_deadline(self, value):
+        if value < timezone.now():
+            raise ValidationError("Deadline cannot be in the past.")
+        return value
+
+
+class SubTaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTask
+        fields = '__all__'
+        read_only_fields = ['created_at']
+
+
+class CategoryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+    def validate_name(self, value):
+        if Category.objects.filter(name=value).exists():
+            raise ValidationError("Category with this name already exists.")
+        return value
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        if Category.objects.filter(name=instance.name).exclude(pk=instance.pk).exists():
+            raise ValidationError("Category with this name already exists.")
+        instance.save()
+        return instance
--- tasks/views.py
+++ tasks/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from django.db.models import Count
 from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Task
-from .serializers import TaskSerializer
+from .models import Task, SubTask
+from .serializers import TaskSerializer, SubTaskCreateSerializer
 
 
 class TaskCreateView(generics.CreateAPIView):
@@ -34,3 +34,13 @@ class TaskStatsView(APIView):
             'overdue_tasks': overdue_tasks
         }
         return Response(data)
+
+
+class SubTaskListCreateView(generics.ListCreateAPIView):
+    queryset = SubTask.objects.all()
+    serializer_class = SubTaskCreateSerializer
+
+
+class SubTaskDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SubTask.objects.all()
+    serializer_class = SubTaskCreateSerializer
